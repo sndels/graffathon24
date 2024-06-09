603dffc537660735142fe2e7d7e2c96280b16fc8
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2024-06-09 12:18:22 +0300

    Fix penrose data push

--- include/penroseTriangles.hpp
+++ include/penroseTriangles.hpp
@@ -1,34 +1,38 @@
 #pragma once
 
-#include "shader.hpp"
 #include "mathTypes.hpp"
+#include "shader.hpp"
 
-
-struct PenroseTriangle {
-    Vec4i   m; // metadata: type, children ids
-    Vec2d   o;
-    Vec2d   u;
-    Vec2d   v;
+struct PenroseTriangle
+{
+    Vec4i m; // metadata: type, children ids
+    Vec2d o;
+    Vec2d u;
+    Vec2d v;
 };
 
-struct TriangleData {
-    Vec4i   m; // metadata: type, children ids
-    Mat2f   uvmInv;
-    Vec2f   o;
+struct TriangleData
+{
+    Vec4i m; // metadata: type, children ids
+    Mat2f uvmInv;
+    Vec2f o;
 };
+static_assert(sizeof(TriangleData) % 16 == 0, "Required for SSBO");
+static_assert(alignof(TriangleData) >= 16, "Required for SSBO");
 
-class PenroseTriangles {
-public:
+class PenroseTriangles
+{
+  public:
     PenroseTriangles();
 
     void subdivideTriangles();
 
     void update(float time);
 
-    void bind(Shader* shader);
+    void bind(Shader *shader);
 
-private:
-    GLuint                          _penroseSsbo;
-    std::vector<PenroseTriangle>    _triangles;
-    std::vector<TriangleData>       _triangleData;
+  private:
+    GLuint _penroseSsbo;
+    std::vector<PenroseTriangle> _triangles;
+    std::vector<TriangleData> _triangleData;
 };
--- src/penroseTriangles.cpp
+++ src/penroseTriangles.cpp
@@ -2,29 +2,29 @@
 
 #include <vector>
 
-
-constexpr double deg18 = (18.0/180.0)*M_PI;
-constexpr double deg36 = (36.0/180.0)*M_PI;
+constexpr double deg18 = (18.0 / 180.0) * M_PI;
+constexpr double deg36 = (36.0 / 180.0) * M_PI;
 constexpr double goldenRatio = 1.618033988749894;
 constexpr double goldenRatioInv = 1.0 / goldenRatio;
-constexpr double goldenRatioInv1 = 1.0-goldenRatioInv;
+constexpr double goldenRatioInv1 = 1.0 - goldenRatioInv;
 
-
-PenroseTriangles::PenroseTriangles() : _triangles(10)
+PenroseTriangles::PenroseTriangles()
+: _triangles(10)
 {
     glGenBuffers(1, &_penroseSsbo);
 
     // Generate lvl0 triangles for penrose sun
-    for (int i=0; i<10; ++i) {
-        auto& triangle = _triangles[i];
-        Vec2d a(cos(deg36*(i+1)), sin(deg36*(i+1)));
-        Vec2d b(cos(deg36*i), sin(deg36*i));
+    for (int i = 0; i < 10; ++i)
+    {
+        auto &triangle = _triangles[i];
+        Vec2d a(cos(deg36 * (i + 1)), sin(deg36 * (i + 1)));
+        Vec2d b(cos(deg36 * i), sin(deg36 * i));
         Vec2d c(0.0, 0.0);
 
         triangle.m << 0, 0, 0, 0;
-        triangle.o = i%2 == 0 ? a : b;
-        triangle.u = i%2 == 0 ? (b-a) : (a-b);
-        triangle.v = i%2 == 0 ? (c-a) : (c-b);
+        triangle.o = i % 2 == 0 ? a : b;
+        triangle.u = i % 2 == 0 ? (b - a) : (a - b);
+        triangle.v = i % 2 == 0 ? (c - a) : (c - b);
     }
 
     subdivideTriangles();
@@ -43,11 +43,13 @@ PenroseTriangles::PenroseTriangles() : _triangles(10)
 void PenroseTriangles::subdivideTriangles()
 {
     auto nTriangles = _triangles.size();
-    for (int i=0; i<nTriangles; ++i) {
-        auto& triangle = _triangles[i];
+    for (int i = 0; i < nTriangles; ++i)
+    {
+        auto &triangle = _triangles[i];
         if (triangle.m(1) > 0) // triangle already has children
             continue;
-        if (triangle.m(0) == 0) { // half-kite
+        if (triangle.m(0) == 0)
+        { // half-kite
             PenroseTriangle t1, t2, t3;
             t1.m << 0, 0, 0, 0;
             t1.o = triangle.o + triangle.u;
@@ -63,16 +65,17 @@ void PenroseTriangles::subdivideTriangles()
             t3.v = triangle.o + triangle.v - t3.o;
 
             triangle.m(1) = _triangles.size();
-            triangle.m(2) = _triangles.size()+1;
-            triangle.m(3) = _triangles.size()+2;
+            triangle.m(2) = _triangles.size() + 1;
+            triangle.m(3) = _triangles.size() + 2;
             _triangles.push_back(std::move(t1));
             _triangles.push_back(std::move(t2));
             _triangles.push_back(std::move(t3));
         }
-        else { // half-dart
+        else
+        { // half-dart
             PenroseTriangle t1, t2;
             t1.m << 0, 0, 0, 0;
-            t1.o = triangle.o + triangle.v*goldenRatioInv1;
+            t1.o = triangle.o + triangle.v * goldenRatioInv1;
             t1.v = triangle.o + triangle.v - t1.o;
             t1.u = triangle.o + triangle.u - t1.o;
             t2.m << 1, 0, 0, 0;
@@ -81,7 +84,7 @@ void PenroseTriangles::subdivideTriangles()
             t2.v = -triangle.u;
 
             triangle.m(1) = _triangles.size();
-            triangle.m(2) = _triangles.size()+1;
+            triangle.m(2) = _triangles.size() + 1;
             _triangles.push_back(std::move(t1));
             _triangles.push_back(std::move(t2));
         }
@@ -104,15 +107,17 @@ void PenroseTriangles::update(float time)
     }
 
     glBindBuffer(GL_SHADER_STORAGE_BUFFER, _penroseSsbo);
+    printf("%d\n", glGetError());
     {
         glBufferData(
             GL_SHADER_STORAGE_BUFFER,
-            _triangleData.size() * sizeof(PenroseTriangle), _triangleData.data(), GL_DYNAMIC_DRAW);
+            _triangleData.size() * sizeof(TriangleData), _triangleData.data(),
+            GL_DYNAMIC_DRAW);
     }
     glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
 }
 
-void PenroseTriangles::bind(Shader* shader)
+void PenroseTriangles::bind(Shader *shader)
 {
     shader->setInt("nTriangles", _triangles.size());
     glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, _penroseSsbo);
